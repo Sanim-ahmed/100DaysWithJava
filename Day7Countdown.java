a5764b8da7f77a18844ca4a8e185ef3f2cb12eee
Sanim ahmed khan <sanimahmedofficial@gmail.com>
2025-09-29 17:16:29 +0600

    Add files via upload

---
+++ Day7Countdown.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day7Countdown {
+    public static void main(String[] args)throws InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Countdown starts from? :");
+        int count = sc.nextInt();
+
+        for (int i = 10;i >0;i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        System.out.println("EID MUBARAK");
+    }
+}
