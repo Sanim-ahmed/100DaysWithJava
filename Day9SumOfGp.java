079f8a65740455c2dec45b23647a1ec1a8121138
Sanim ahmed khan <sanimahmedofficial@gmail.com>
2025-10-01 19:30:38 +0600

    Add files via upload

---
+++ Day9SumOfGp.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Day9SumOfGp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();//First number of the series
+        int d = sc.nextInt();//Number diff of the series
+        int n = sc.nextInt();//last number of ther series
+        int sum =0;
+
+        int i ;
+        for (i = a; i <= n; i *=d ) {
+            System.out.print(i+" ");
+            sum +=i;
+            
+        }
+        System.out.println();
+        System.out.println(sum);
+    }
+}
