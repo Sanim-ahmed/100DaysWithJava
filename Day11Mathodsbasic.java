25feb3540821ebfdae79c72407efdf2807aa1d05
Sanim ahmed khan <sanimahmedofficial@gmail.com>
2025-10-07 23:53:37 +0600

    Create Day11Mathodsbasic.java

---
+++ Day11Mathodsbasic.java
@@ -0,0 +1,17 @@
+public class Day11Mathodsbasic{
+    public static void main(String[] args) {
+
+        String name = "Sanim khan";
+        int age = 22;
+
+        kaifahaluka(name, age);
+
+    }
+    static  void kaifahaluka(String name,int age) {
+
+        System.out.println("Amer name "+name);
+        System.out.println();
+        System.out.println("amr boyosh "+age);
+    }
+    }
+
